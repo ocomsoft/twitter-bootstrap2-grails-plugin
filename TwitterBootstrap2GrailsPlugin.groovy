cac2dace345786bb824cc415bfc57e2bca5d0fe2
Scott Warren <scottwarren@ocom.com.au>
2014-10-08 12:42:43 +1100

    Renamed Plugin....

--- TwitterBootstrap2GrailsPlugin.groovy
+++ TwitterBootstrap2GrailsPlugin.groovy
@@ -1,4 +1,4 @@
-class TwitterBootstrapGrailsPlugin {
+class TwitterBootstrap2GrailsPlugin {
 
     // the plugin version
     def version = "2.3.2.3"
@@ -9,14 +9,14 @@ class TwitterBootstrapGrailsPlugin {
     def developers = [ [ name: "Nicholas Wittstruck", email: "nicholas@spotmapping.com" ]]
     def organization = [ name: "GroovyDev", url: "http://groovydev.com/" ]
     def title = "Plugin Twitter Bootstrap CSS framework resource files"
-    def description = """Provides Twitter Bootstrap CSS framework resource files.
+    def description = """Provides Twitter Bootstrap 2.3.2 CSS framework resource files.
 
 Bootstrap is Twitter's toolkit for kickstarting CSS for websites, apps, and more. It includes base CSS styles for typography, forms, buttons, tables, grids, navigation, alerts, and more.
 To get started with Bootstrap checkout the [Bootstrap Documentation|http://twitter.github.com/bootstrap]!
 """
 
-    def documentation = "https://github.com/groovydev/twitter-bootstrap-grails-plugin/blob/master/README.md"
+    def documentation = "https://github.com/ocomsoft/twitter-bootstrap2-grails-plugin/blob/master/README.md"
     def license = "APACHE"
-    def issueManagement = [ system: "github", url: "https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues" ]
-    def scm = [ url: "https://github.com/groovydev/twitter-bootstrap-grails-plugin" ]
+    def issueManagement = [ system: "github", url: "https://github.com/ocomsoft/twitter-bootstrap2-grails-plugin/issues" ]
+    def scm = [ url: "https://github.com/ocomsoft/twitter-bootstrap2-grails-plugin" ]
 }
